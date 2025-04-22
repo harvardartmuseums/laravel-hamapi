6ade2c9b8465b2a40f616e0fe1ea9817c8184c9e
Ian Callahan <code@greatfortress.com>
2025-04-22 10:50:20 -0400

    Temporal order for publications

--- src/Classes/HamPublication.php
+++ src/Classes/HamPublication.php
@@ -18,4 +18,24 @@ class HamPublication extends HamClass
         $this->isinshop = rawurlencode($isinshop);
         return $this;
     }
+
+    public function getNext($publication_id)
+    {
+        $this->after = ['temporalorder' => $publication_id];
+        $this->size = 1;
+        $this->sort = 'temporalorder';
+        $this->sortorder = 'asc';
+
+        return $this;
+    }
+
+    public function getPrevious($publication_id)
+    {
+        $this->before = ['temporalorder' => $publication_id];
+        $this->size = 1;
+        $this->sort = 'temporalorder';
+        $this->sortorder = 'desc';
+
+        return $this;
+    }
 }
